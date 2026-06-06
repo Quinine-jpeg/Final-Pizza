bc3e37ba009218f02655d6e6eab1285be9bb9a01
Quinn Fryer <quinn.fryer@wycliffe.nsw.edu.au>
2026-06-06 12:07:50 +1000

    Added js and style files from SSA app
    Intending to change font and colour scheme etc later

---
+++ static/app.js
@@ -0,0 +1,43 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("static/js/serviceWorker.js")
+        .then((res) => console.log("service worker registered"))
+        .catch((err) => console.log("service worker not registered", err));
+    });
+  }
+
+const error = document.getElementById('error');
+const pw = document.getElementById('pw');
+const submit = document.getElementById('submit');
+
+function validate() {
+  submit.disabled = true;
+  if (pw.value.length < 8) {
+    error.textContent = 'Password must be 8 characters or longer.'; return;
+  } else if (!/[A-Z]/.test(pw.value) || !/[a-z]/.test(pw.value)) {
+    error.textContent = 'Password must contain a capital and lowercase letter.'; return;
+  } else if (!/\d/.test(pw.value)) {
+    error.textContent = 'Password must contain a number'; return;
+  } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(pw.value)) {
+    error.textContent = 'Password must contain a special character'; return;
+  } else {
+    submit.disabled = false;
+    error.textContent = '';
+  }
+}
+
+if (pw) {pw.addEventListener('input', validate)}
+
+function encode(event) {
+  event.target.value = event.target.value.replace(/['"<>/\\]/g, '');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const inputs = document.querySelectorAll('input');
+  
+  inputs.forEach(function(input) {
+    input.addEventListener('input', encode);
+    console.log('Listener applied to:', input)
+  });
+});
---
+++ static/serviceworker.js
@@ -0,0 +1,56 @@
+const assets = [
+    "/",
+    "static/css/style.css",
+    "static/js/app.js",
+    "static/images/logo.png",
+    "static/images/favicon.jpg",
+    "static/images/icon-128x128.png",
+    "static/images/icon-192x192.png",
+    "static/images/icon-384x384.png",
+    "static/images/icon-512x512.png"
+  ];
+
+const CATALOGUE_ASSETS = "catalogue-assets";
+
+self.addEventListener("install", (installEvt) => {
+  installEvt.waitUntil(
+    caches
+      .open(CATALOGUE_ASSETS)
+      .then((cache) => {
+        console.log(cache)
+        cache.addAll(assets);
+      })
+      .then(self.skipWaiting())
+      .catch((e) => {
+        console.log(e);
+      })
+  );
+});
+
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches
+      .keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key === CATALOGUE_ASSETS) {
+              console.log("Removed old cache from", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", function (evt) {
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CATALOGUE_ASSETS).then((cache) => {
+        return cache.match(evt.request);
+      });
+    })
+  );
+})
